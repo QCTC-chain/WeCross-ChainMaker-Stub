9c1f89b39151c9cab1674816cfbea9a85f5f3ab8
shaxunyeman <shaxunyeman@gmail.com>
2025-05-21 17:37:01 +0800

    修改注册EVM合约的时候需要提供 abi 信息

--- src/main/java/com/webank/wecross/stub/chainmaker/custom/RegisterContractHandler.java
+++ src/main/java/com/webank/wecross/stub/chainmaker/custom/RegisterContractHandler.java
@@ -1,9 +1,11 @@
 package com.webank.wecross.stub.chainmaker.custom;
 
+import com.webank.wecross.exception.WeCrossException;
 import com.webank.wecross.stub.*;
 import com.webank.wecross.stub.chainmaker.ChainMakerConnection;
 import com.webank.wecross.stub.chainmaker.common.ChainMakerConstant;
 import com.webank.wecross.stub.chainmaker.common.ChainMakerStatusCode;
+import com.webank.wecross.stub.chainmaker.utils.ConfigUtils;
 import org.chainmaker.pb.common.ContractOuterClass;
 import org.chainmaker.pb.config.ChainConfigOuterClass;
 import org.chainmaker.sdk.ChainClientException;
@@ -13,11 +15,22 @@ import org.chainmaker.sdk.utils.CryptoUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterContractHandler implements CommandHandler {
     private Logger logger = LoggerFactory.getLogger(RegisterContractHandler.class);
+
+    private boolean isExistsABI(String configPath, String contractName) throws WeCrossException {
+        try {
+            ConfigUtils.getContractABI(configPath, contractName);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
     public void handle(
             Path path,
             Object[] args,
@@ -40,9 +53,7 @@ public class RegisterContractHandler implements CommandHandler {
                     chainMakerConnection.getProperties().get(ChainMakerConstant.CHAINMAKER_CHAIN_ID));
             resourceProperties.put(
                     ChainMakerConstant.CHAINMAKER_CONTRACT_ADDRESS,
-                    CryptoUtils.nameToAddrStr(
-                            (String) resourceInfo.getProperties().get(ChainMakerConstant.CHAINMAKER_CONTRACT_NAME),
-                            ChainConfigOuterClass.AddrType.CHAINMAKER));
+                    contract.getName());
             resourceProperties.put(
                     ChainMakerConstant.CHAINMAKER_CONTRACT_VERSION,
                     contract.getVersion());
@@ -50,8 +61,26 @@ public class RegisterContractHandler implements CommandHandler {
                     ChainMakerConstant.CHAINMAKER_CONTRACT_RUNTIME_TYPE,
                     contract.getRuntimeType().name());
             resourceInfo.setProperties(resourceProperties);
+
+            if(!isExistsABI(chainMakerConnection.getConfigPath(), contract.getName())) {
+                // args 最后一个参数是命令
+                if(args.length == 1) {
+                    callback.onResponse(
+                            new TransactionException(
+                                    ChainMakerStatusCode.HandleGetContracts,
+                                    String.format("请提供 %s 合约的 ABI", contract.getName())),
+                            null);
+                    return;
+                } else {
+                    ConfigUtils.writeContractABI(
+                            chainMakerConnection.getConfigPath(),
+                            contract.getName(),
+                            (String) args[0]);
+                }
+            }
             chainMakerConnection.getConnectionEventHandler().onANewResource(resourceInfo);
-        } catch (ChainMakerCryptoSuiteException | ChainClientException e) {
+            callback.onResponse(null, resourceProperties);
+        } catch (ChainMakerCryptoSuiteException | ChainClientException | WeCrossException | IOException e) {
             logger.error("获取 {} 合约信息失败。{}", path.getResource(), e.getMessage());
             callback.onResponse(
                     new TransactionException(
